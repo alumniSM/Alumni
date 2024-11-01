81b0757c9418c48e7d39800ac371fb99637cf166
alumniSM <simplifyweb03@gmail.com>
2024-11-01 17:41:19 +0300

    Update api.js

--- Alumni_2/api.js
+++ Alumni_2/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api", // adjust this to your backend URL
+  baseURL: "https://alumni-backend-zwtv.onrender.com", // adjust this to your backend URL
 });
 
 api.interceptors.request.use(
